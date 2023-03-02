c109db3a1d15982b32792c9225b9aef7038ce0fe
Kevin McDonnell <ktm@cs.stonybrook.edu>
2023-03-01 19:28:22 -0500

    fix

--- tests/unit_tests.h
+++ tests/unit_tests.h
@@ -11,7 +11,7 @@
 
 #define TEST_INPUT_DIR "tests.in"
 #define TEST_OUTPUT_DIR "tests.out"
-#define TEST_TIMEOUT 30 
+#define TEST_TIMEOUT 5 
 
 int run_using_system(char *test_name);
 int run_using_system_no_valgrind(char *test_name);
@@ -19,4 +19,6 @@ void expect_normal_exit(int status);
 void expect_error_exit(int status);
 void expect_no_valgrind_errors(int status);
 void expect_outfile_matches(char *name);
-void print_char_array(char *chars, int chars_len);
+void report_return_value(int return_act, int return_exp);
+void report_message(char *actual, char *expected);
+void report_packets(unsigned char *actual[], char *expected[], unsigned int *packet_lens_exp, unsigned int num_expected);
